a2138e8a816ec6558b069c18d0c926775e07bfaf
Kartikey Semwal <55513264+kartikeysemwal@users.noreply.github.com>
2021-03-26 11:55:15 +0530

    Registration of user and admin

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require("express");
-
 const app = express();
 
-app.use("/", (req, res) => {
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => {
     res.send("Welcome to luck draw");
 });
+
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
