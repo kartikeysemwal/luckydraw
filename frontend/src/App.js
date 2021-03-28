578f81d56c7e96ea5cd3d54500420c1e4c9613cb
Kartikey Semwal <55513264+kartikeysemwal@users.noreply.github.com>
2021-03-28 14:37:30 +0530

    Added register screen for user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
 import BookScreen from "./screens/BookScreen";
 import UserScreen from "./screens/UserScreen";
 import EventScreen from "./screens/EventScreen";
@@ -20,6 +21,7 @@ function App() {
             <main className="py-3">
                 <Container>
                     <Route path="/login" component={LoginScreen} />
+                    <Route path="/register" component={RegisterScreen} />
                     <Route
                         path="/bookticket/:ticketId"
                         component={BookScreen}
